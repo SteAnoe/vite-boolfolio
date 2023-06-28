3daa0cdb3db1069a1a0f2d94f64dda72d6373c78
SteAnoe <ste.anoe@gmail.com>
2023-06-28 15:38:04 +0200

    finito

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,39 @@
-<script >
-
+<script>
+import axios from 'axios';
+import ProjectCard from './components/ProjectCard.vue'
+export default{
+  components: {
+    ProjectCard
+  },
+  data(){
+    return{
+      projects:[],
+      baseUrl: 'http://127.0.0.1:8000'
+    }
+},
+mounted(){
+	this.getProjects();
+},
+methods:{
+	getProjects(){
+		axios.get(`${this.baseUrl}/api/projects`)
+		.then(res=>{
+		this.projects=res.data.projects
+		})
+	}
+}
+}
 </script>
 
 <template>
-  <h1>Ciao mondo</h1>
+  <div class="container d-flex flex-wrap">
+    <ProjectCard v-for="(elem,index) in projects" :key="index" :propsProject="elem"/>
+  </div>
 </template>
 
 <style lang="scss">
 @use './style/main.scss';
+.d-flex{
+  gap: 40px;
+}
 </style>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,46 @@
+<script>
+export default{
+        name: "ProjectCard",
+        props: ['propsProject'],
+    data(){
+        return{
+          
+          baseUrl: 'http://127.0.0.1:8000'
+        }
+    },
+}
+</script>
+<h3></h3>
+<template>
+<div class="card" style="width: 18rem;">
+  <img :src="`${baseUrl}/storage/${propsProject.img}`" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Title <br>{{ propsProject.name }}</h5>
+    <p class="card-text">Description: <br>{{ propsProject.description }}</p>
+    <p class="card-text">Client: {{ propsProject.client }}</p>
+    <p class="card-text">Project Type: {{propsProject.type.name}}</p>
+    <ul id="mario" class="card-text"> 
+        Technology used:
+        <li v-for="(elem,index) in propsProject.technologies" :key="index" id="pino">{{elem.name}}</li>
+    </ul>
+  </div>
+</div>
+</template>
+
+<style lang="scss">
+.card{
+    width: 100px;
+}
+#mario{
+    padding-left: 0;
+    #pino{
+        text-decoration: underline;
+    }
+}
+*{
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+</style>
